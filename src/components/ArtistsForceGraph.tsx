3c4cfb47346c0dec13d98437d94c305982c7a4d5
Sean Simmons <seanrobinsimmons@gmail.com>
2025-09-18 08:36:25 -0400

    slightly more zoomed out

--- src/components/ArtistsForceGraph.tsx
+++ src/components/ArtistsForceGraph.tsx
@@ -140,7 +140,7 @@ const ArtistsForceGraph: React.FC<ArtistsForceGraphProps> = ({
             // Reheat the simulation when data or visibility changes
             fgRef.current.d3ReheatSimulation?.();
             const isMobile = window.matchMedia('(max-width: 640px)').matches
-            fgRef.current.zoom(isMobile ? 0.12 : 0.22)
+            fgRef.current.zoom(isMobile ? 0.12 : 0.18)
         }
     }, [preparedData, show]);
 
