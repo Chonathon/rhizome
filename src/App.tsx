731338c1a6749f2c69a0da2e726e558f076009b9
b-price <bprice12@students.towson.edu>
2025-11-09 13:45:51 -0500

    navigating back from selected genre and clicking on top artist now properly navigates to the artist

--- src/App.tsx
+++ src/App.tsx
@@ -948,6 +948,7 @@ function App() {
       }
     }
 
+    setGraph('artists');
     setSelectedArtistFromSearch(false);
     setArtistPreviewStack([]);
     setSelectedArtist(artist); // For graph focus/dimming
